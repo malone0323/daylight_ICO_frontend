98879159821e55fb343d010d7df505c29b882f0a
roosevelt47r <roosevelt47rempellil@hotmail.com>
2024-10-02 18:08:03 +0200

    !

--- src/Blocks/Progress/index.jsx
+++ src/Blocks/Progress/index.jsx
@@ -5,13 +5,13 @@ import "./style.css";
 const ProgressSecction = () => {
   return (
     <div className="progress-container ">
-      <Option check title="Whitelist Registration" date="25th July, 2022" />
-      <Option check title="Whitelist Concluded" date="14th August, 3PM UTC" />
-      <Option check title="Presale Round 1" date="14th August, 3PM UTC" />
+      <Option check title="Whitelist Registration" date="25th Jan, 2025" />
+      <Option check title="Whitelist Concluded" date="14th May, 3PM UTC" />
+      <Option check title="Presale Round 1" date="14th May, 3PM UTC" />
       <Option
         check
         title="Presale Conclusion & Token Distribution"
-        date="21st August, 3PM UTC"
+        date="21st May, 3PM UTC"
       />
       <Option check title="Launch & IDO" date="To Be Announced" />
     </div>
